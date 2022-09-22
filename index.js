bcb7c98221b8428f6bb431f68972556dc2a081f4
Leonardo Ayala Grados <leonardoayala03072000@hotmail.com>
2022-09-21 23:05:08 -0500

    Delete de items dinamico agregado

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 console.log("buenas noches");
 class Dato {
+
     constructor(detalle, dinero) {
         this._detalle = detalle;
         this._dinero = dinero;
@@ -11,9 +12,12 @@ class Dato {
 }
 
 class Ingreso extends Dato {
+    static id = 0;
     constructor(detalle, dinero) {
         super(detalle, dinero)
+        this._idIngreso = ++Ingreso.id;
     }
+    get getIdIngreso() { return this._idIngreso }
 }
 class Egreso extends Dato {
     constructor(detalle, dinero) {
@@ -22,42 +26,41 @@ class Egreso extends Dato {
 }
 
 // manejo de array en clases
-const arregloIngreso = [new Ingreso('mochila', 60),
-    new Ingreso('tijera', 2)
-];
+const arregloIngreso = [];
 
 function agregarDinero() {
     let detalle = document.getElementById('detalle');
     let monto = document.getElementById('monto');
     console.log("Detalle : " + detalle.value + " el monto ingresado es : " + parseInt(monto.value));
     let nuevoIngreso = new Ingreso(detalle.value, monto.value);
-    let montoAgregado = arregloIngreso.push(nuevoIngreso);
-    console.log("++++++" + montoAgregado);
-
+    arregloIngreso.push(nuevoIngreso);
     mostrarInformacion();
 }
 
 function mostrarInformacion() {
     let total = 0;
     let texto = '';
-    for (let iterador of arregloIngreso) {
-        let mensaje = "El detalle es  : " + iterador._detalle + " -dinero : " + iterador._dinero;
 
-        console.log(mensaje);
-        total += parseInt(iterador._dinero);
+    if (arregloIngreso.length === 0) {
+        document.getElementById('informacionEgreso').innerHTML = '<h3 class="text-center">no existen registros</h3>';
+        console.log('if longitud del arreglo' + arregloIngreso.length);
+    } else if (arregloIngreso.length > 0) {
+        for (let iterador of arregloIngreso) {
+            total += parseInt(iterador._dinero);
+            texto += ` <li class="list-group-item d-flex justify-content-between align-items-center">${iterador._idIngreso}: ${iterador._detalle} 
+                        <span class="badge badge- badge-primary">s/ ${iterador._dinero}</span>
+                        <button onclick="eliminarItemIngreso(${iterador._idIngreso})" class="btn btn-danger">x</button></li>`;
+            document.getElementById('informacionEgreso').innerHTML = texto;
+            console.log('longitud del arreglo' + arregloIngreso.length);
 
-        console.log("El total : " + total);
-        texto += ` 
-        <li class="list-group-item d-flex justify-content-between align-items-center">${iterador._detalle} 
-        <span class="badge badge- badge-primary">s/ ${iterador._dinero}</span>
-        <button onclick="prueba()" class="btn btn-danger">-</button></li>`;
-
-        document.getElementById('informacionEgreso').innerHTML = texto;
+        }
     }
     document.getElementById('montoActual').innerHTML = ` ${total} soles `;
     console.log(arregloIngreso);
 }
 
-function prueba() {
-    alert();
+function eliminarItemIngreso(id) {
+    let itemEliminado = arregloIngreso.findIndex(ingreso => ingreso._idIngreso === id);
+    arregloIngreso.splice(itemEliminado, 1); //el indice que se va a eliminar y luego ahi cuantos otros itemas se van a eliminar
+    mostrarInformacion();
 }
